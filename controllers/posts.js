71b99ae1f05d2969849e59c1ee002900586a5c05
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-17 02:04:26 +0800

    add feature edit update and delete post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -21,6 +22,24 @@ export const createPost = async (req, res) => {
     }
 }
 
+
 export const updatePost = async(req, res) => {
+    const {id: _id } = req.params;
+    const post = req.body;
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+
+    res.json(updatedPost);
+}
+
+
+export const deletePost = async (req, res) => {
     const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({message: 'Post deleted Successfully'});
+      
 }
