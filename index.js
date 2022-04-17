92deb04666e3ad257e2b70550d8efe54621fcaaf
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-17 15:29:07 +0800

    apply dotenv

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/post.js';
-
+import dotenv from 'dotenv'
 const app =  express();
-
+dotenv.config();
 app.use(bodyParser.json({limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true }));
 app.use(cors());
@@ -13,10 +13,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL =  'mongodb+srv://juneroy:juneroy@cluster0.nagrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL =  'mongodb+srv://juneroy:juneroy@cluster0.nagrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT =  process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error));
 
