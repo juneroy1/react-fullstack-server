fb8b5daaf997e783daead49e12c927287f2b91a5
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-21 02:01:16 +0800

    add feature authentication full functions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/post.js';
+import userRoutes from './routes/users.js';
 import dotenv from 'dotenv'
 const app =  express();
 dotenv.config();
@@ -11,12 +12,13 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 
-// const CONNECTION_URL =  'mongodb+srv://juneroy:juneroy@cluster0.nagrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL =  'mongodb+srv://juneroy:juneroy@cluster0.nagrm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT =  process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error));
 
