aa712c18bc78921a26ace5cce6ffb87752cfaeff
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-23 19:05:16 +0800

    add feature view post details

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,16 @@ import e from "express";
 import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
+export const getPost = async (req, res) => {
+    const {id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message});
+    }
+}
 export const getPosts = async (req, res) => {
     const {page} = req.query;
     try {
--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { getPostsBySearch, getPosts, createPost, updatePost , deletePost, likePost} from "../controllers/posts.js";
+import {getPost, getPostsBySearch, getPosts, createPost, updatePost , deletePost, likePost} from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 const router = express.Router();
-router.get('/', getPosts);
+
 router.get('/search', getPostsBySearch);
+router.get('/:id', getPost);
+router.get('/', getPosts);
+
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
